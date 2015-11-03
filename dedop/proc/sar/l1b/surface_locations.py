a130671fe4e8c917f2c2ec6384654f8f09af0d48
mark-ep <mark.pattle@isardsat.co.uk>
2015-11-03 12:01:55 +0000

    Surface Location Algorithm
    created initial implementation of the surface location algorithm

---
+++ dedop/proc/sar/l1b/surface_locations.py
@@ -0,0 +1,91 @@
+# import tables as tab
+import numpy as np
+from numpy.linalg import norm
+from ...geo.lla2ecef import lla2ecef
+
+
+class SurfaceLocationAlgorithm:
+    def __init__(self):
+        self.first_surf = False
+        self.new_surf = False
+
+        self.time_surf = 0
+        self.x_surf = 0
+        self.y_surf = 0
+        self.z_surf = 0
+        self.lat_surf = 0
+        self.lon_surf = 0
+        self.alt_surf = 0
+
+    def store_first_location(self, isps):
+        isp_record = isps[-1]
+
+        self.time_surf = isp_record.time_sar_ku
+
+        self.x_surf = isp_record.x_sar_surf
+        self.y_surf = isp_record.y_sar_surf
+        self.z_surf = isp_record.z_sar_surf
+
+        self.lat_surf = isp_record.lat_sar_sat
+        self.lon_surf = isp_record.lon_sar_sat
+        self.alt_surf = isp_record.alt_sar_sat -\
+                        isp_record.win_delay_sar_ku * cst.c / 2.
+
+    def process_surface_location(self, locs, isps):
+        if not locs:
+            self.first_surf = True
+            self.new_surf = True
+
+            self.store_first_location(isps)
+        else:
+            self.first_surf = False
+            self.new_surf = self.find_new_location(locs, isps)
+
+    def find_new_location(self, locs, isps):
+        surface = locs[-1]
+        isp_curr = isps[-1]
+        isp_prev = isps[-2]
+
+        ground_surf_orbit_vector = np.matrix(
+            [[isp_curr.x_sar_surf - surface.x_sat],
+             [isp_curr.y_sar_surf - surface.y_sat],
+             [isp_curr.z_sar_surf - surface.z_sat]]
+        )
+        ground_surf_orbit_angle = np.arccos(
+            np.dot(isp_curr.surf_sat_vector, ground_surf_orbit_vector) /
+            (norm(isp_curr.surf_sat_vector) * norm(ground_surf_orbit_vector))
+        )
+        if ground_surf_orbit_angle < surface.angular_azimuth_beam_resolution:
+            return False
+
+        ground_surf_orbit_vector_prev = np.matrix(
+            [[isp_prev.x_sar_surf - surface.x_sat],
+             [isp_prev.y_sar_surf - surface.y_sat],
+             [isp_prev.z_sar_surf - surface.z_sat]]
+        )
+        ground_surf_orbit_angle_prev = np.arccos(
+            np.dot(isp_prev.surf_sat_vector, ground_surf_orbit_vector_prev) /
+            (norm(isp_prev.surf_sat_vector) * norm(ground_surf_orbit_vector_prev))
+        )
+
+        alpha = (surface.angular_azimuth_beam_resolution - ground_surf_orbit_angle_prev) /\
+                (ground_surf_orbit_angle - ground_surf_orbit_angle_prev)
+
+        self.time_surf = surface.time_sar_ku +\
+            alpha * (isp_curr.time_sar_ku - isp_prev.time_sar_ku)
+        self.x_surf = surface.x_sar_surf +\
+            alpha * (isp_curr.x_sar_surf - isp_prev.x_sar_surf)
+        self.y_surf = surface.y_sar_surf +\
+            alpha * (isp_curr.y_sar_surf - isp_prev.y_sar_surf)
+        self.z_surf = surface.z_sar_surf +\
+            alpha * (isp_curr.z_sar_surf - isp_prev.z_sar_surf)
+
+        surf_loc_cart = np.matrix([[self.x_surf],
+                                   [self.y_surf],
+                                   [self.z_surf]])
+        surf_loc_geod = lla2ecef(surf_loc_cart)
+        self.lat_surf = surf_loc_geod[0]
+        self.lon_surf = surf_loc_geod[1]
+        self.alt_surf = surf_loc_geod[2]
+
+        return True
