ba7250a232e7c422b7ca5ecd77505044382847d4
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-11-23 13:47:27 +0100

    added docstrings

--- dedop/gui/__init__.py
+++ dedop/gui/__init__.py
@@ -1,1 +1,16 @@
+"""
+A graphical user interface for the DDP.
+"""
+
 __author__ = 'DeDop Development Team'
+
+
+from dedop.gui.mainwindow import MainWindow
+from dedop.gui.io import IO
+from dedop.gui.preferences import Preferences
+
+__all__ = [
+    'MainWindow',
+    'Preferences',
+    'IO'
+]
