964e7453b9b3639cf94064b9dca0a4aaf214f529
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-04-14 10:25:49 +0100

    update conf tests with respect to refactor of CST and CHD

--- tests/conf/test_cst.py
+++ tests/conf/test_cst.py
@@ -20,42 +20,42 @@ class TestCST(unittest.TestCase):
 
     def test_c(self):
         expected = self.expected["c_cst"]
-        actual = self.actual.c.value
+        actual = self.actual.c
 
         self.assertAlmostEqual(expected, actual)
 
     def test_semi_major_axis(self):
         expected = self.expected["semi_major_axis_cst"]
-        actual = self.actual.semi_major_axis.value
+        actual = self.actual.semi_major_axis
 
         self.assertAlmostEqual(expected, actual)
 
     def test_earth_radius(self):
         expected = self.expected["earth_radius_cst"]
-        actual = self.actual.earth_radius.value
+        actual = self.actual.earth_radius
 
         self.assertAlmostEqual(expected, actual)
 
     def test_sec_in_day(self):
         expected = self.expected["sec_in_day_cst"]
-        actual = self.actual.sec_in_day.value
+        actual = self.actual.sec_in_day
 
         self.assertAlmostEqual(expected, actual)
 
     def test_pi(self):
         expected = self.expected["pi_cst"]
-        actual = self.actual.pi.value
+        actual = self.actual.pi
 
         self.assertAlmostEqual(expected, actual)
 
     def test_flat_coeff(self):
         expected = self.expected["flat_coeff_cst"]
-        actual = self.actual.flat_coeff.value
+        actual = self.actual.flat_coeff
 
         self.assertAlmostEqual(expected, actual)
 
     def test_semi_minor_axis(self):
         expected = self.expected["semi_minor_axis_cst"]
-        actual = self.actual.semi_minor_axis.value
+        actual = self.actual.semi_minor_axis
 
         self.assertAlmostEqual(expected, actual)
