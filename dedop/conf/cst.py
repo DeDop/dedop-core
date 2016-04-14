fb67bff4a24bd8488d0dc5c146b83a29ff02d086
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-04-14 10:25:00 +0100

    refactor to allow direct access to values of CST/CHD fields

--- dedop/conf/cst.py
+++ dedop/conf/cst.py
@@ -11,37 +11,37 @@ class ConstantsFile(ConstantsFileReader):
     @property
     def c(self):
         """Speed of light"""
-        return self["c_cst"]
+        return self["c_cst"].value
 
     @property
     def semi_major_axis(self):
         """Semi-major axis of WGS84 ellipsoid"""
-        return self["semi_major_axis_cst"]
+        return self["semi_major_axis_cst"].value
 
     @property
     def earth_radius(self):
         """Earth Radius"""
-        return self["earth_radius_cst"]
+        return self["earth_radius_cst"].value
 
     @property
     def sec_in_day(self):
         """Number of seconds in a day"""
-        return self["sec_in_day_cst"]
+        return self["sec_in_day_cst"].value
 
     @property
     def pi(self):
         """Pi number"""
-        return self["pi_cst"]
+        return self["pi_cst"].value
 
     @property
     def flat_coeff(self):
         """Flattening coefficient of WGS84 ellipsoid"""
-        return self["flat_coeff_cst"]
+        return self["flat_coeff_cst"].value
 
     @property
     def semi_minor_axis(self):
         """Semi-minor axis of WGS84 ellipsoid"""
-        return self["semi_minor_axis_cst"]
+        return self["semi_minor_axis_cst"].value
 
     def __init__(self, filename=None):
         if filename is None:
