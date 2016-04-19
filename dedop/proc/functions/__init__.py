6f03e8615f886ed7fae8cb14f895083bc0cad6de
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-04-19 12:24:27 +0100

    fixed error in 'angle_between' function

--- dedop/proc/functions/__init__.py
+++ dedop/proc/functions/__init__.py
@@ -6,5 +6,5 @@ __all__ = ["angle_between"]
 def angle_between(vec1, vec2):
     return np.arccos(
         np.dot(vec1, vec2) /
-        norm(vec1) * norm(vec2)
+        (norm(vec1) * norm(vec2))
     )
