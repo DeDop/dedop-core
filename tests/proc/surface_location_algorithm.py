c3e30f87eb2d57d084ffe163673f2e1d0985db0d
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-04-19 15:14:32 +0100

    updated surface location algorithm tests to reflect changes to InstrumentSourcePacket and SurfaceLocationData

--- tests/proc/surface_location_algorithm.py
+++ tests/proc/surface_location_algorithm.py
@@ -13,21 +13,17 @@ class SurfaceLocationAlgorithmTests(unittest.TestCase):
     expected_01 = "proc/surface_location_algorithm/surface_location_algorithm_01/expected/"\
                   "Hr_Algorithms.Surface_Location_Algorithm_Processing_01.Expected_01.txt"
     input_01 = "proc/surface_location_algorithm/surface_location_algorithm_01/input/"\
-               "input_isps.txt"
+               "inputs.txt"
 
     expected_02 = "proc/surface_location_algorithm/surface_location_algorithm_02/expected/" \
                   "Hr_Algorithms.Surface_Location_Algorithm_Processing_02.Expected_01.txt"
-    input_02_isps = "proc/surface_location_algorithm/surface_location_algorithm_02/input/" \
-                    "input_isps.txt"
-    input_02_surfs = "proc/surface_location_algorithm/surface_location_algorithm_02/input/" \
-                    "input_surfs.txt"
+    input_02 = "proc/surface_location_algorithm/surface_location_algorithm_02/input/" \
+               "inputs.txt"
 
     expected_03 = "proc/surface_location_algorithm/surface_location_algorithm_03/expected/" \
                   "Hr_Algorithms.Surface_Location_Algorithm_Processing_03.Expected_01.txt"
-    input_03_isps = "proc/surface_location_algorithm/surface_location_algorithm_03/input/" \
-                    "input_isps.txt"
-    input_03_surfs = "proc/surface_location_algorithm/surface_location_algorithm_03/input/" \
-                     "input_surfs.txt"
+    input_03 = "proc/surface_location_algorithm/surface_location_algorithm_03/input/" \
+               "inputs.txt"
 
     def setUp(self):
         self.cst = ConstantsFile(self.constants_file)
@@ -35,13 +31,41 @@ class SurfaceLocationAlgorithmTests(unittest.TestCase):
         self.surface_location_algorithm = SurfaceLocationAlgorithm(self.chd, self.cst)
 
     def test_surface_location_algorithm_01(self):
-        expected_data = TestDataLoader(self.expected_01, delim=' ')
-        isps = list(MockObject.load_file(self.input_01, delim=' '))
+        """
+        surface location algorithm test 01
+        ----------------------------------
 
+        loads an input ISP and passes it as an initial
+        item to the surface location algorithm.
+
+        expected result is for the algorithm to create
+        an initial surface location. Values of this
+        location are validated.
+        """
+        # load expected data
+        expected_data = TestDataLoader(self.expected_01, delim=' ')
+        # load input data
+        isp = TestDataLoader(self.input_01, delim=' ')
+
+        # create ISP object
+        isps = [
+            InstrumentSourcePacket(
+                self.cst, self.chd,
+                time_sar_ku=isp["time_sar_ku"],
+                lat_sar_sat=isp["lat_sar_sat"],
+                lon_sar_sat=isp["lon_sar_sat"],
+                alt_sar_sat=isp["alt_sar_sat"],
+                win_delay_sar_ku=isp["win_delay_sar_ku"]
+            )
+        ]
+
+        # execute surface location algorithm
         new_surf = self.surface_location_algorithm([], isps)
 
+        # confirm new surface is created
         self.assertTrue(new_surf, msg="failed to create new surface")
 
+        # retreive & validate surface properties
         surf = self.surface_location_algorithm.get_surface()
 
         self.assertAlmostEqual(surf["time_surf"], expected_data["time_surf"])
@@ -55,34 +79,115 @@ class SurfaceLocationAlgorithmTests(unittest.TestCase):
         self.assertAlmostEqual(surf["alt_surf"], expected_data["alt_surf"])
 
     def test_surface_location_algorithm_02(self):
-        isps = list(MockObject.load_file(self.input_02_isps, delim=' '))
-        isps = [InstrumentSourcePacket(i, self.cst, **isp) for i, isp in enumerate(isps)]
+        """
+        surface location algorithm test 02
+        ----------------------------------
 
-        surf = next(MockObject.load_file(self.input_02_surfs, delim=' '))
-        surf.update(isps[0].__dict__)
-        surfs = [SurfaceLocationData(surf, self.cst, self.chd)]
+        loads multiple input ISPs and one input surface location.
+        expected result is for the surface location algorithm to
+        determine that a new surface location should not yet be
+        calculated
+        """
+        # load input data
+        inputs = TestDataLoader(self.input_02, delim=' ')
 
-        new_surf = self.surface_location_algorithm(surfs, isps)
+        # generate input ISP objects
+        isps = [
+            InstrumentSourcePacket(self.cst, self.chd, i,
+                                   time_sar_ku=time,
+                                   lat_sar_sat=inputs["lat_sar_sat"][i],
+                                   lon_sar_sat=inputs["lon_sar_sat"][i],
+                                   alt_sar_sat=inputs["alt_sar_sat"][i],
+                                   win_delay_sar_ku=inputs["win_delay_sar_ku"][i])\
+                for i, time in enumerate(inputs["time_sar_ku"])
+        ]
+        for isp in isps:
+            isp.compute_location_sar_surf()
 
+        # create prior surface location object
+        surf = SurfaceLocationData(self.cst, self.chd,
+            time_surf=inputs["time_surf"],
+            x_surf=inputs["x_surf"],
+            y_surf=inputs["y_surf"],
+            z_surf=inputs["z_surf"],
+            x_sat=inputs["x_sat"],
+            y_sat=inputs["y_sat"],
+            z_sat=inputs["z_sat"],
+            x_vel_sat=inputs["x_vel_sat"],
+            y_vel_sat=inputs["y_vel_sat"],
+            z_vel_sat=inputs["z_vel_sat"]
+        )
+        surf.compute_surf_sat_vector()
+        surf.compute_angular_azimuth_beam_resolution(
+            inputs["pri_sar_pre_dat"]
+        )
+
+        # execute surface location algorithm
+        new_surf = self.surface_location_algorithm([surf], isps)
+
+        # confirm that no new surface is generated
         self.assertFalse(new_surf, msg="erroneously created new surface")
 
     def test_surface_location_algorithm_03(self):
+        """
+        surface location algorithm test 03
+        ----------------------------------
+
+        loads multiple input ISPs and one input surface location.
+        expected result is for the surface location algorithm to
+        generate a new surface location. The attributes of this
+        new surface location are then validated against the expected
+        values.
+        """
+        # load the expected data
         expected_data = TestDataLoader(self.expected_03, delim=' ')
 
-        isps = list(MockObject.load_file(self.input_03_isps, delim=' '))
-        isps = [InstrumentSourcePacket(i, self.cst, **isp) for i, isp in enumerate(isps)]
+        # load the input data
+        inputs = TestDataLoader(self.input_03, delim=' ')
 
-        surfs = list(MockObject.load_file(self.input_03_surfs, delim=' '))
-        for surf in surfs:
-            surf.update(isps[0].__dict__)
-        surfs = [SurfaceLocationData(surf, self.cst, self.chd) for surf in surfs]
+        # create all input ISP objects
+        isps = [
+            InstrumentSourcePacket(self.cst, self.chd, i,
+                                   time_sar_ku=time,
+                                   lat_sar_sat=inputs["lat_sar_sat"][i],
+                                   lon_sar_sat=inputs["lon_sar_sat"][i],
+                                   alt_sar_sat=inputs["alt_sar_sat"][i],
+                                   win_delay_sar_ku=inputs["win_delay_sar_ku"][i]) \
+            for i, time in enumerate(inputs["time_sar_ku"])
+            ]
+        # calculate surface position for each ISP
+        for isp in isps:
+            isp.compute_location_sar_surf()
 
-        new_surf = self.surface_location_algorithm(surfs, isps)
+        # create the prior surface location object
+        surf = SurfaceLocationData(self.cst, self.chd,
+                                   time_surf=inputs["time_surf"],
+                                   x_surf=inputs["x_surf"],
+                                   y_surf=inputs["y_surf"],
+                                   z_surf=inputs["z_surf"],
+                                   x_sat=inputs["x_sat"],
+                                   y_sat=inputs["y_sat"],
+                                   z_sat=inputs["z_sat"],
+                                   x_vel_sat=inputs["x_vel_sat"],
+                                   y_vel_sat=inputs["y_vel_sat"],
+                                   z_vel_sat=inputs["z_vel_sat"]
+                                   )
+        # compute properties of the surface location
+        surf.compute_surf_sat_vector()
+        surf.compute_angular_azimuth_beam_resolution(
+            inputs["pri_sar_pre_dat"]
+        )
 
+        # execute the surface location algorithm
+        new_surf = self.surface_location_algorithm([surf], isps)
+
+        # confirm new surface has been created
         self.assertTrue(new_surf, msg="failed to create new surface")
 
+        # retreive properties of the surface location
         surf = self.surface_location_algorithm.get_surface()
 
+        # validate properties
         self.assertAlmostEqual(surf["time_surf"][0, 0], expected_data["time_surf"])
 
         self.assertAlmostEqual(surf["x_surf"][0, 0], expected_data["x_surf"], delta=1e-5)
