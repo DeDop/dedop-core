de3459358880525ff9712916dd51c596dd30b2e4
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-04-20 14:20:46 +0100

    added docstrings to BaseAlgorithm

--- dedop/proc/sar/base_algorithm.py
+++ dedop/proc/sar/base_algorithm.py
@@ -3,6 +3,20 @@ from ...util.parameter import Parameter
 @Parameter('n_looks_stack', data_type=float)
 @Parameter('zp_fact_range', data_type=float)
 class BaseAlgorithm:
+    """
+    THhe base class from which all other algorithm classes
+    inherit properties.
+
+    This class is also used to define configuration
+    parameters which are shared between multiple
+    algorithms.
+    """
     def __init__(self, chd, cst):
+        """
+        Initialise the BaseAlgorithm instance
+
+        :param chd: the CHD data object
+        :param cst: the CST data object
+        """
         self.chd = chd
         self.cst = cst
