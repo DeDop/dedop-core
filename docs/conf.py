0c3231d7053a61c881cd35d8b7fa635dbb9a387f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-06-03 17:09:49 +0200

    changed doc style

--- docs/conf.py
+++ docs/conf.py
@@ -120,9 +120,10 @@ todo_include_todos = True
 # a list of builtin themes.
 #html_theme = 'alabaster'
 #html_theme = 'pyramid'
-html_theme = 'sphinxdoc'
+#html_theme = 'sphinxdoc'
 #html_theme = 'nature'
 #html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
