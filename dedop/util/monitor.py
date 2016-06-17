b242a1e9f675a82bb43c6b907dfeb61b83cfc98c
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-06-17 16:32:46 +0200

    added cancel method

--- dedop/util/monitor.py
+++ dedop/util/monitor.py
@@ -4,7 +4,7 @@ This module defines the :py:class:`Monitor` interface that may be used by functi
 that offer support for observation and control of long-running tasks.
 
 Example usage:::
-    from dedop.util.monitor import starting
+    from ect.core.util.monitor import starting
 
     def long_running_task(a, b, c, monitor):
         with monitor.starting('doing a long running task', total_work=100)
@@ -27,6 +27,7 @@ The module also provides a simple but still useful default implementation :py:cl
 prints progress output directly to the console.
 
 """
+import signal
 from abc import ABCMeta, abstractmethod
 from contextlib import contextmanager
 
@@ -34,7 +35,7 @@ from contextlib import contextmanager
 class Monitor(metaclass=ABCMeta):
     """
     A monitor is used to both observe and control a running operation.
-    Pass ``Monitor.NULL`` to DeDop API functions that expect a monitor instead of passing ``None``.
+    Pass ``Monitor.NULL`` to functions that expect a monitor instead of passing ``None``.
 
     The ``Monitor`` class is an abstract base class and clients must implement the following three abstract methods:
     :py:meth:`start`, :py:meth:`progress`, and :py:meth:`done`.
@@ -96,6 +97,31 @@ class Monitor(metaclass=ABCMeta):
         """
         return ChildMonitor(self, work)
 
+    def cancel(self):
+        """
+        Request the task to be cancelled.
+        This method will be usually called from the code that created the monitor, not by users of the monitor.
+        For example, a GUI could create the monitor due to an invocation of a long-running task, and then
+        the user wishes to cancel that task.
+        The default implementation does nothing.
+        Override to implement something useful.
+        """
+        pass
+
+    def is_cancelled(self) -> bool:
+        """
+        Check if there is an external request to cancel the current task observed by this monitor.
+
+        Users of a monitor shall frequently call this method and check its return value.
+        If cancellation is requested, they should politely exit the current processing in a proper way, e.g.
+        by cleaning up allocated resources.
+        The default implementation returns ``False``.
+        Subclasses shall override this method to return ``True`` if a task cancellation request was detected.
+
+        :return: ``True`` if task cancellation was requested externally. The default implementation returns ``False``.
+        """
+        return False
+
 
 class _NullMonitor(Monitor):
     def __repr__(self):
@@ -115,7 +141,7 @@ class _NullMonitor(Monitor):
         return Monitor.NULL
 
 
-#: Pass ``Monitor.NULL`` to DeDop API functions that expect a monitor instead of passing ``None``.
+#: Pass ``Monitor.NULL`` to functions that expect a monitor instead of passing ``None``.
 Monitor.NULL = _NullMonitor()
 
 
@@ -151,14 +177,24 @@ class ChildMonitor(Monitor):
         parent_work = 0.0 if self._total_work is not None else None
         self._parent_monitor.progress(work=parent_work, msg=self._label)
 
+    def cancel(self):
+        self._parent_monitor.cancel()
+
+    def is_cancelled(self) -> bool:
+        return self._parent_monitor.is_cancelled()
+
 
 class ConsoleMonitor(Monitor):
-    """A naive monitor that directly writes to stdout."""
+    """
+    A simple console monitor that directly writes to stdout and detects cancellation requests via CTRL+C.
+    """
 
     def __init__(self):
         self._label = None
         self._worked = None
         self._total_work = None
+        self._cancelled = False
+        self._old_ctrl_c_handler = False
 
     def start(self, label: str, total_work: float = None):
         if not label:
@@ -168,6 +204,7 @@ class ConsoleMonitor(Monitor):
         self._total_work = total_work
         line = '%s: start' % self._label
         self.write_line(line)
+        self._old_ctrl_c_handler = signal.signal(signal.SIGINT, self._on_ctrl_c)
 
     def progress(self, work: float = None, msg: str = None):
         percentage = None
@@ -187,7 +224,18 @@ class ConsoleMonitor(Monitor):
     def done(self):
         line = '%s: done' % self._label
         self.write_line(line)
+        signal.signal(signal.SIGINT, self._old_ctrl_c_handler)
 
     # noinspection PyMethodMayBeStatic
     def write_line(self, line):
         print(line)
+
+    def cancel(self):
+        self._cancelled = True
+
+    def is_cancelled(self) -> bool:
+        return self._cancelled
+
+    def _on_ctrl_c(self, signal, frame):
+        print('You pressed Ctrl+C!')
+        self.cancel()
