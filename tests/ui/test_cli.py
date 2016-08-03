74c64f8afe5595cbc33d8adc1494dc3312711065
Hans Permana <hans.permana@gmail.com>
2016-08-03 15:49:38 +0200

    v0.5.1

--- tests/ui/test_cli.py
+++ tests/ui/test_cli.py
@@ -53,7 +53,7 @@ class CliTest(WorkspaceTestBase, TestCase):
         self._test_main(['-h'], expected_stdout='usage: dedop [-h]')
 
     def test_option_version(self):
-        self._test_main(['--version'], expected_stdout='0.5.0')
+        self._test_main(['--version'], expected_stdout='0.5.1')
 
     def test_command_none(self):
         self._test_main([], expected_stdout='usage: dedop [-h]')
