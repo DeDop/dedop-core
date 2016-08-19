ffda9f1bb88e38f839dc3112058a50aa27894cf9
Mark Pattle <mark.pattle@isardsat.co.uk>
2016-08-19 10:48:35 +0100

    re-configured structure of Auxiliary file reading

--- dedop/conf/characterization.py
+++ dedop/conf/characterization.py
@@ -1,126 +1,105 @@
-from .constants_reader import *
+from .auxiliary_file_reader import *
 from .constants import ConstantsFile
 
 from typing import Any
 
-class CharacterisationFile(ConstantsFileReader):
+
+class CharacterisationFile(AuxiliaryFileReader):
     """
     class for loading the Characterisation File
     """
-    
-    @property
-    def pitch_bias(self) -> float:
-        """Pitch bias measured in-flight"""
-        return self["pitch_bias_chd"]
-    
-    @property
-    def n_samples_sar(self) -> int:
-        """Number of samples per each SAR pulse"""
-        return self["N_samples_sar_chd"]
+    _id = "CHD"
 
-    @property
-    def power_tx_ant_ku(self) -> float:
-        """Antenna SSPA RF Peak Transmitted Power in Ku band"""
-        return self["power_tx_ant_ku_chd"]
-
-    @property
-    def onboard_proc_sar_raw(self) -> float:
-        """Combination of on-board fixed digital gains for the SAR Raw digital
-            chain from ADC converter to TM (it includes ADC, FFTs, beta)"""
-        return self["onboard_proc_sar_raw_chd"]
-    
-    @property
-    def yaw_bias(self) -> float:
-        """Yaw bias measured in-flight"""
-        return self["yaw_bias_chd"]
-
-    @property
-    def freq_ku(self) -> float:
-        """Emitted frequency in Ku-band"""
-        return self["freq_ku_chd"]
-
-    @property
-    def rfu_rx_gain_ground(self) -> float:
-        """Combination of RFU gains in the receiving chain"""
-        return self["rfu_rx_gain_ground_chd"]
-    
-    @property
-    def pulse_length(self) -> float:
-        """Pulse length"""
-        return self["pulse_length_chd"]
-
-    @property
-    def bw_ku(self) -> float:
-        """Ku-band bandwidth"""
-        return self["bw_ku_chd"]
-    
-    @property
-    def n_ku_pulses_burst(self) -> int:
-        """Number of Ku-band pulses per burst"""
-        return self["N_ku_pulses_burst_chd"]
-    
-    @property
-    def cai_cor2_unit_conv(self) -> float:
-        """Factor that converts CAI to COR2 units"""
-        return self["cai_cor2_unit_conv_chd"]
-
-    @property
-    def h0_cor2_unit_conv(self) -> float:
-        """Factor that converts HO to COR2 units"""
-        return self["h0_cor2_unit_conv_chd"]
-    
-    @property
-    def i_sample_start(self) -> int:
-        """The RMC ASIC truncates the time domain burst and keeps only the range
-            gates with indexes going from (i_sample_start_chd) to
-            (i_sample_start_chd + N_samples_sar_chd/2-1)"""
-        return self["i_sample_start_chd"]
-    
-    @property
-    def fai_shift_number(self) -> float:
-        """Number of fai shifts to complete one sample in LRM and SAR science
-            waveforms"""
-        return self["fai_shift_number_chd"]
-    
-    @property
-    def t0_h0_unit_conv(self) -> float:
-        """Factor that converts T0 to H0 units"""
-        return self["T0_h0_unit_conv_chd"]
-    
-    @property
-    def antenna_gain_ku(self) -> float:
-        """Antenna gain for Ku-band"""
-        return self["antenna_gain_ku_chd"]
-
-    @property
-    def roll_bias(self) -> float:
-        """Roll bias measured in-flight"""
-        return self["roll_bias_chd"]
-
-    @property
-    def pri_T0_unit_conv(self) -> float:
-        """Factor that converts PRI to T0 units"""
-        return self["pri_T0_unit_conv_chd"]
-
-    @property
-    def uso_freq_nom(self) -> float:
-        """uso freq nom value"""
-        return self["uso_freq_nom_chd"]
-
-    @property
-    def alt_freq_multiplier(self) -> float:
-        """alt freq multiplier value"""
-        return self["alt_freq_multiplier_chd"]
-
-    @property
-    def pri_sar(self) -> float:
-        """pulse repetition interval"""
-        return self["pri_sar_chd"]
-
-    @property
-    def t0_nom(self) -> float:
-        """nominal value for T0"""
-        return 1. / (self.uso_freq_nom * self.alt_freq_multiplier)
+    pitch_bias = AuxiliaryParameter(
+        "pitch_bias_chd",
+        """pitch bias measured in-flight""",
+        param_type=float)
+    n_samples_sar = AuxiliaryParameter(
+        "N_samples_sar_chd",
+        """number of samples per each SAR pulse""",
+        param_type=int)
+    power_tx_ant_ku = AuxiliaryParameter(
+        "power_tx_ant_ku_chd",
+        """antenna SSPA RF peak transmitted power in Ku band""",
+        param_type=float)
+    onboard_proc_sar_raw = AuxiliaryParameter(
+        "onboard_proc_sar_raw_chd",
+        """combination of on-board fixed digital gains for the SAR raw digital
+            chain from ADC converter to TM (it includes ADC, FFTs, beta)""",
+        param_type=float)
+    yaw_bias = AuxiliaryParameter(
+        "yaw_bias_chd",
+        """yaw bias measured in-flight""",
+        param_type=float)
+    freq_ku = AuxiliaryParameter(
+        "freq_ku_chd",
+        """emitted frequency in Ku-band""",
+        param_type=float)
+    rfu_rx_gain_ground = AuxiliaryParameter(
+        "rfu_rx_gain_ground_chd",
+        """combination of RFU gains in the receiving chain""",
+        param_type=float)
+    pulse_length = AuxiliaryParameter(
+        "pulse_length_chd",
+        """pulse length""",
+        param_type=float)
+    bw_ku = AuxiliaryParameter(
+        "bw_ku_chd",
+        """Ku-band bandwidth""",
+        param_type=float)
+    n_ku_pulses_burst = AuxiliaryParameter(
+        "N_ku_pulses_burst_chd",
+        """number of ku-band pulses per burst""",
+        param_type=int)
+    cai_cor2_unit_conv = AuxiliaryParameter(
+        "cai_cor2_unit_conv_chd",
+        """factor that converts CAI to COR2 units""",
+        param_type=float)
+    h0_cor2_unit_conv = AuxiliaryParameter(
+        "h0_cor2_unit_conv_chd",
+        """factor that converts H0 to COR2 units""",
+        param_type=int)
+    i_sample_start = AuxiliaryParameter(
+        "i_sample_start_chd",
+        """the RMC ASIC truncates the time domain burst and keeps only
+            the range gates with indexes going from (i_sample_start) to
+            (i_sample_start_chd + N_samples_sar_chd // 2-1)""",
+        param_type=int)
+    fai_shift_number = AuxiliaryParameter(
+        "fai_shift_number_chd",
+        """number of fai shifts to complete one sample in LRM and SAR
+            science waveforms""",
+        param_type=int)
+    t0_h0_unit_conv = AuxiliaryParameter(
+        "T0_h0_unit_conv_chd",
+        """factor that converts T0 to H0 units""",
+        param_type=float)
+    antenna_gain_ku = AuxiliaryParameter(
+        "antenna_gain_ku_chd",
+        """antenna gain for ku-band""")
+    roll_bias = AuxiliaryParameter(
+        "roll_bias_chd",
+        """roll bias measured in-flight""",
+        param_type=float)
+    pri_T0_unit_conv = AuxiliaryParameter(
+        "pri_T0_unit_conv_chd",
+        """factor for converting from PRI to T0 units""",
+        param_type=float)
+    uso_freq_nom = AuxiliaryParameter(
+        "uso_freq_nom_chd",
+        """USO frequency nominal value""",
+        param_type=float)
+    alt_freq_multiplier = AuxiliaryParameter(
+        "alt_freq_multiplier_chd",
+        param_type=float)
+    pri_sar = AuxiliaryParameter(
+        "pri_sar_chd",
+        """pulse repetition interval""",
+        param_type=float)
+    mean_sat_alt = AuxiliaryParameter(
+        "mean_sat_alt_chd",
+        """mean altitude of the satellite""",
+        param_type=float)
 
     @property
     def chirp_slope_ku(self) -> float:
@@ -137,11 +116,13 @@ class CharacterisationFile(ConstantsFileReader):
         return self._wv_length_ku
 
     @property
-    def mean_sat_alt(self) -> float:
-        """mean satellite altitude"""
-        return self["mean_sat_alt_chd"]
-    
+    def t0_nom(self):
+        return 1. / (self.uso_freq_nom * self.alt_freq_multiplier)
+
     def __init__(self, cst: ConstantsFile, filename: str=None, **kwargs: Any):
+        """
+        create a new CharacterisationFile object
+        """
         self._wv_length_ku = kwargs.pop(
             'wv_length_ku_chd', None
         )
@@ -150,5 +131,4 @@ class CharacterisationFile(ConstantsFileReader):
         )
 
         self.cst = cst
-
         super().__init__(filename, **kwargs)
