fix problems with stack masking

dedop/model/l1a_processing_data.py
@@ -243,60 +243,46 @@ class L1AProcessingData:
         """
         The x_vel_sat_sar property of the packet
         """
-        return self["x_vel_sat_sar"]
+        return self._vel_sat_sar[0, 0]
 
     @x_vel_sat_sar.setter
     def x_vel_sat_sar(self, value):
-        self["x_vel_sat_sar"] = value
-
-    @x_vel_sat_sar.deleter
-    def x_vel_sat_sar(self):
-        del self["x_vel_sat_sar"]
+        self._vel_sat_sar[0, 0] = value
 
     @property
     def y_vel_sat_sar(self):
         """
         The y_vel_sat_sar property of the packet
         """
-        return self["y_vel_sat_sar"]
+        return self._vel_sat_sar[1, 0]
 
     @y_vel_sat_sar.setter
     def y_vel_sat_sar(self, value):
-        self["y_vel_sat_sar"] = value
-
-    @y_vel_sat_sar.deleter
-    def y_vel_sat_sar(self):
-        del self["y_vel_sat_sar"]
+        self._vel_sat_sar[1, 0] = value
 
     @property
     def z_vel_sat_sar(self):
         """
         The z_vel_sat_sar property of the packet
         """
-        return self["z_vel_sat_sar"]
+        return self.vel_sat_sar[2, 0]
 
     @z_vel_sat_sar.setter
     def z_vel_sat_sar(self, value):
-        self["z_vel_sat_sar"] = value
-
-    @z_vel_sat_sar.deleter
-    def z_vel_sat_sar(self):
-        del self["z_vel_sat_sar"]
+        self._vel_sat_sar[2, 0] = value
 
     @property
     def vel_sat_sar(self):
         """
         The Lat, Lon, and Alt of the satellite position
         """
-        return np.asmatrix([self.x_vel_sat_sar,
-                            self.y_vel_sat_sar,
-                            self.z_vel_sat_sar]).T
+        return self._vel_sat_sar
 
     @vel_sat_sar.setter
     def vel_sat_sar(self, value):
-        self.x_vel_sat_sar, \
-         self.y_vel_sat_sar, \
-         self.z_vel_sat_sar = value
+        self._vel_sat_sar[0, 0] = value[0]
+        self._vel_sat_sar[1, 0] = value[1]
+        self._vel_sat_sar[2, 0] = value[2]
 
     @property
     def roll_sar(self):
@@ -754,6 +740,10 @@ class L1AProcessingData:
         self._beam_angles_trend = None
         self._burst_processed = False
         self._vel_sat_norm = None
+        x_vel = values.pop('x_vel_sat_sar', 0)
+        y_vel = values.pop('y_vel_sat_sar', 0)
+        z_vel = values.pop('z_vel_sat_sar', 0)
+        self._vel_sat_sar = np.matrix([[x_vel], [y_vel], [z_vel]], dtype=np.float64)
 
         self.isp_pid = PacketPid.null
 

dedop/proc/sar/algorithms/multilooking.py
@@ -225,7 +225,7 @@ class MultilookingAlgorithm(BaseAlgorithm):
             (n_samples_max,), dtype=np.float64
         )
         self.sample_counter = np.zeros(
-            (n_samples_max,), dtype=int
+            (n_samples_max,), dtype=np.float64
         )
 
         max_stack = min(self.n_looks_stack, surface.data_stack_size)
@@ -243,10 +243,9 @@ class MultilookingAlgorithm(BaseAlgorithm):
                 continue
 
             if self.flag_avoid_zeros_in_multilooking:
-                beam_mask = surface.stack_mask[beam_index, :]
-                mask = ne.evaluate("where(beam_mask == 0, 0, 1)")
+               mask = surface.stack_mask[beam_index, :]
             else:
-                mask = 1
+               mask = 1
             self.waveform_multilooked[:] += weighted_beams[beam_index, :] * mask
             self.sample_counter[:] += mask
 

dedop/proc/sar/algorithms/range_compression.py
@@ -1,14 +1,10 @@
 import numpy as np
-from dedop.conf import CharacterisationFile, ConstantsFile, ConfigurationFile
+from numpy.fft import fft, fftshift
 
 from dedop.model import SurfaceData
 from ..base_algorithm import BaseAlgorithm
 
 class RangeCompressionAlgorithm(BaseAlgorithm):
-
-    def __init__(self, chd: CharacterisationFile, cst: ConstantsFile, cnf: ConfigurationFile):
-        super().__init__(chd, cst, cnf)
-
     def __call__(self, working_surface_location: SurfaceData) -> None:
         # calc. size after zero padding factor applied
         padded_size = self.zp_fact_range * self.chd.n_samples_sar
@@ -24,20 +20,22 @@ class RangeCompressionAlgorithm(BaseAlgorithm):
             dtype=np.complex128
         )
 
-        # calc. FFT with zero-padding & orthogonal scaling
-        beam_fft = np.fft.fft(
-            working_surface_location.beams_geo_corr, axis=1,
-            n=padded_size, norm='ortho'
-        )
-        # apply shift
-        beam_shift = np.fft.fftshift(beam_fft, axes=1)[:stack_size]
-        # TODO: REMOVE THIS !!
-        # disable extra shift to fix alignment problem
-        # beam_shift = beam_fft
+        for beam_index in range(stack_size):
 
-        # store complex result
-        self.beam_range_compr_iq[:stack_size, :] = beam_shift
+            # calc. FFT with zero-padding & orthogonal scaling
+            beam_fft = fft(
+                working_surface_location.beams_geo_corr[beam_index, :],
+                n=padded_size, norm="ortho"
+            )
+            # apply shift
+            beam_shift = fftshift(beam_fft)
+            # TODO: REMOVE THIS !!
+            # disable extra shift to fix alignment problem
+            # beam_shift = beam_fft
 
-        # compute square modulus
-        self.beam_range_compr[:stack_size, :] =\
-            np.abs(beam_shift) ** 2
+            # store complex result
+            self.beam_range_compr_iq[beam_index, :] = beam_shift
+
+            # compute square modulus
+            self.beam_range_compr[beam_index, :] =\
+                np.abs(beam_shift) ** 2

dedop/proc/sar/algorithms/stack_masking.py
@@ -6,7 +6,6 @@ from ..base_algorithm import BaseAlgorithm
 from ....util.parameter import Parameter
 
 
-@Parameter("flag_avoid_zeros_in_multilooking", data_type=bool)
 @Parameter("flag_stack_masking", data_type=bool)
 class StackMaskingAlgorithm(BaseAlgorithm):
 
@@ -46,7 +45,7 @@ class StackMaskingAlgorithm(BaseAlgorithm):
     def compute_geometry_mask(self, working_surface_location: SurfaceData) -> np.ndarray:
         geom_mask = np.zeros(
             (self.n_looks_stack, self.chd.n_samples_sar * self.zp_fact_range),
-            dtype=np.int8
+            dtype=np.float64
         )
         max_stack = min(working_surface_location.data_stack_size, self.n_looks_stack)
         for beam_index in range(max_stack):
@@ -71,7 +70,7 @@ class StackMaskingAlgorithm(BaseAlgorithm):
     def compute_ambiguity_mask(self, working_surface_location: SurfaceData) -> np.ndarray:
         ambi_mask = np.zeros(
             (self.n_looks_stack, self.chd.n_samples_sar * self.zp_fact_range),
-            dtype=np.int8
+            dtype=np.float64
         )
         ## TODO: to be defined
         ambi_mask[:, :] = 1
@@ -80,8 +79,8 @@ class StackMaskingAlgorithm(BaseAlgorithm):
 
     def combine_masks(self, geom_mask: np.ndarray, ambig_mask: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         stack_size, beam_size = geom_mask.shape
-        stack_mask = np.zeros((stack_size, beam_size))
-        stack_mask_vector = np.zeros((stack_size))
+        stack_mask = np.zeros((stack_size, beam_size), dtype=np.float64)
+        stack_mask_vector = np.zeros((stack_size), dtype=np.float64)
 
         for beam_index in range(stack_size):
             stack_mask[beam_index, :] = geom_mask[beam_index, :] * ambig_mask[beam_index, :]
@@ -117,11 +116,5 @@ class StackMaskingAlgorithm(BaseAlgorithm):
     def apply_mask(self, working_surface_location: SurfaceData, stack_mask: np.ndarray) -> np.ndarray:
         output = working_surface_location.beams_range_compr *\
                  stack_mask[:working_surface_location.data_stack_size, :]
-
-        if self.flag_avoid_zeros_in_multilooking:
-            invalid = (stack_mask == 0)
-            invalid =\
-                invalid[:working_surface_location.data_stack_size, :]
-            output[invalid] = np.NaN
 
         return output