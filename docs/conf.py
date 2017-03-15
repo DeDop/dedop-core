5f3d7d46d4db676198e524fcd109f813b5ecb67f
Hans Permana <hans.permana@gmail.com>
2017-03-15 09:25:51 +0100

    split the RTD env file, mock cate-util

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,14 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+# packages thare are difficult to import, becuase the collide with pillow==3.0.0 requirement by rtd
+# http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+import mock
+
+MOCK_MODULES = ['cate-util']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
