d49b115a24d57fb1dd81f19ec3258df295f776ec
Hans Permana <hans.permana@gmail.com>
2017-03-22 11:49:39 +0100

    update doc

--- dedop/conf/__init__.py
+++ dedop/conf/__init__.py
@@ -4,9 +4,11 @@ Processor configuration management.
 
 __author__ = 'DeDop Development Team'
 
-# __all__ = [
-#     'characterization.py', 'constants.py', 'configuration.py'
-# ]
+__all__ = [
+    'characterization.py',
+    'constants.py',
+    'configuration.py'
+]
 
 from .characterization import CharacterisationFile
 from .configuration import ConfigurationFile
