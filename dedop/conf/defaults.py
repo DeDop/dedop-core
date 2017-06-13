f5420b53680115853ffb57716129505d2eab2dbc
Hans Permana <hans.permana@gmail.com>
2017-06-13 15:07:09 +0200

    webapi.log is now inside the version directory

--- dedop/conf/defaults.py
+++ dedop/conf/defaults.py
@@ -1,15 +1,17 @@
 import os.path
+from ..version import __version__
 
 DEFAULT_DATA_DIR_NAME = '.dedop'
 DEFAULT_DATA_PATH = os.path.join(os.path.expanduser('~'), DEFAULT_DATA_DIR_NAME)
 DEFAULT_WORKSPACE_NAME = 'default'
 DEFAULT_WORKSPACE_PATH = os.path.join(DEFAULT_DATA_PATH, 'workspaces', DEFAULT_WORKSPACE_NAME)
+DEFAULT_VERSION_DATA_PATH = os.path.join(DEFAULT_DATA_PATH, __version__)
 
 #: allow a 100 ms period between two progress messages sent to the client
 WEBAPI_PROGRESS_DEFER_PERIOD = 0.5
 
 #: where a running WebAPI service logs to
-WEBAPI_LOG_FILE_PREFIX = os.path.join(DEFAULT_DATA_PATH, 'webapi.log')
+WEBAPI_LOG_FILE_PREFIX = os.path.join(DEFAULT_VERSION_DATA_PATH, 'webapi.log')
 
 #: By default, WebAPI service will auto-exit after 2 hours of inactivity, if WebAPI auto-exit enabled
 WEBAPI_ON_INACTIVITY_AUTO_STOP_AFTER = 120 * 60.0
