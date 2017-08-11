46563f2535fc0ec719e3ff61f1196e38c1dea7ac
Mark Pattle <mark.pattle@isardsat.co.uk>
2017-08-11 13:31:07 +0100

    updates to sigma0 method

--- dedop/proc/sar/algorithms/sigma_zero_scaling.py
+++ dedop/proc/sar/algorithms/sigma_zero_scaling.py
@@ -2,6 +2,7 @@ from math import sqrt, log10
 import numpy as np
 
 from dedop.model import SurfaceData
+from dedop.conf.enums import AzimuthWindowingMethod
 from ..base_algorithm import BaseAlgorithm
 
 
@@ -17,10 +18,7 @@ class Sigma0ScalingFactorAlgorithm(BaseAlgorithm):
         """
 
         # TODO: come back to this and replace it with something less messy
-        if self.n_looks_stack is not None:
-            max_stack = min(working_surface_location.data_stack_size, self.n_looks_stack)
-        else:
-            max_stack = working_surface_location.data_stack_size  # TODO
+        max_stack = min(working_surface_location.data_stack_size, self.cnf.n_looks_stack)
 
         self.sigma0_scaling_factor_beam = np.zeros(
             (max_stack,), dtype=np.float64
@@ -28,8 +26,7 @@ class Sigma0ScalingFactorAlgorithm(BaseAlgorithm):
 
         sigma0_offset = 10 * log10(64) - \
             10 * log10(self.chd.power_tx_ant_ku) - 2 * self.chd.antenna_gain_ku +\
-            10 * log10(self.chd.n_samples_sar * self.zp_fact_range) -\
-            10 * log10(self.chd.pulse_length * self.chd.pulse_length * chirp_slope_ku)
+            30 * log10(self.cst.pi)
 
         for beam_index in range(max_stack):
             range_sat_surf = working_surface_location.range_sat_surf[beam_index]
@@ -42,7 +39,7 @@ class Sigma0ScalingFactorAlgorithm(BaseAlgorithm):
 
             with np.errstate(divide='ignore'):
                 azimuth_distance =\
-                    (1 + range_sat_surf) / self.cst.earth_radius *\
+                    (1 + range_sat_surf / self.cst.earth_radius) *\
                     wavelength_ku * range_sat_surf / pri_sar_pre_dat /\
                     (2 * vel_sat_sar_norm * self.chd.n_ku_pulses_burst)
             range_distance = 2 * sqrt(
@@ -50,11 +47,18 @@ class Sigma0ScalingFactorAlgorithm(BaseAlgorithm):
                 (self.chd.pulse_length * chirp_slope_ku) *
                 self.cst.earth_radius / (self.cst.earth_radius + range_sat_surf)
             )
-            surface_area = azimuth_distance * range_distance
+
+            # TODO: apply factor based on window width?
+            widening_factor = {
+                AzimuthWindowingMethod.hamming: 1.486 * .92,
+                AzimuthWindowingMethod.hanning: 1.0,  # fixme: add correct value
+                AzimuthWindowingMethod.boxcar: 1.0,  # fixme: add correct value
+                AzimuthWindowingMethod.disabled: 1.0
+            }[self.cnf.flag_azimuth_windowing_method]
+            surface_area = widening_factor * azimuth_distance * range_distance * .886
 
             self.sigma0_scaling_factor_beam[beam_index] = sigma0_offset +\
-                30 * log10(self.cst.pi) + 40 * log10(range_sat_surf) -\
-                20 * log10(wavelength_ku) - 10 * log10(surface_area)
+                40 * log10(range_sat_surf) - 20 * log10(wavelength_ku) - 10 * log10(surface_area)
 
         self.sigma0_scaling_factor = np.mean(self.sigma0_scaling_factor_beam)
         return self.sigma0_scaling_factor, self.sigma0_scaling_factor_beam
