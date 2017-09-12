fix doppler angle calculation

dedop/proc/functions/vectors.py
@@ -6,7 +6,9 @@ def angle_between(vec1: np.ndarray, vec2: np.ndarray) -> float:
     """
     get the angle between two vectors
     """
-    return np.arccos(
-        np.dot(vec1, vec2) /
-        (norm(vec1) * norm(vec2))
-    )[0, 0]
+    arg = np.dot(vec1.flat, vec2.flat) /\
+          (norm(vec1) * norm(vec2))
+    if arg > 1:
+        arg = 1
+
+    return np.arccos(arg)