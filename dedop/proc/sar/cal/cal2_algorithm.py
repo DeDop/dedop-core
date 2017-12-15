3eeb132edc70c7dfaaa5b59ed3dc8050249b36de
Mark Pattle <mark.pattle@isardsat.co.uk>
2017-12-15 13:42:39 +0000

    corrections to cal algorithms

--- dedop/proc/sar/cal/cal2_algorithm.py
+++ dedop/proc/sar/cal/cal2_algorithm.py
@@ -9,7 +9,7 @@ class CAL2Algorithm(BaseAlgorithm):
     def __call__(self, burst: L1AProcessingData) -> None:
         if self.cnf.flag_cal2_correction:
             pulses = np.ones((self.chd.n_ku_pulses_burst, 1))
+            correction = burst.cal2_array[np.newaxis, :] * pulses
 
-            aux = fftshift(fft(burst.waveform_cor_sar, self.chd.n_samples_sar, 2), 2)
-            burst.waveform_cor_sar = ifft(ifftshift(aux * (pulses * burst.cal2_array),
-                                                    2), self.chd.n_samples_sar, 2)
+            wfm_fft = fftshift(fft(burst.waveform_cor_sar, self.chd.n_samples_sar, 1), 1)
+            burst.waveform_cor_sar = ifft(ifftshift(wfm_fft / np.sqrt(correction), 1), self.chd.n_samples_sar, 1)
