7d1d609c14ed3f2a03f17cb3e9890f7caad6626f
Hans Permana <hans.permana@gmail.com>
2018-02-08 10:49:28 +0100

    workaround for https://github.com/DeDop/dedop-core/issues/46

--- dedop/cli/__init__.py
+++ dedop/cli/__init__.py
@@ -1,1 +1,13 @@
 __author__ = 'DeDop Development Team'
+
+
+def _fix_46():
+    import os.path
+    import sys
+    # See https://github.com/DeDop/dedop-core/issues/46
+    extra_path = os.path.join(sys.prefix, 'site-packages')
+    if os.path.isdir(extra_path) and extra_path not in sys.path:
+        sys.path.append(extra_path)
+
+
+_fix_46()
